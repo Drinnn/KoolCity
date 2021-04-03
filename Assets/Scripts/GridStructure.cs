8cff28ce2bde5c8d2521f08fdd607623bab6c011
Drinnn <pedro_lontro@hotmail.com>
2021-04-03 18:50:14 -0300

    refactor: split grid structure into a separate class

---
+++ Assets/Scripts/GridStructure.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+public class GridStructure {
+    private int _cellSize = 3;
+
+    public Vector3 CalculateGridPosition(Vector3 inputPosition) {
+        int x = Mathf.FloorToInt((float)inputPosition.x / _cellSize);
+        int z = Mathf.FloorToInt((float)inputPosition.z / _cellSize);
+
+        return new Vector3(x * _cellSize, 0, z * _cellSize);
+    }
+
+}
--- Assets/Scripts/InputManager.cs
+++ Assets/Scripts/InputManager.cs
@@ -3,9 +3,8 @@ using UnityEngine.EventSystems;
 
 public class InputManager : MonoBehaviour {
     [SerializeField] private LayerMask groundLayerMask;
-    [SerializeField] private GameObject buildingPrefab;
 
-    private int _cellSize = 3;
+    // [SerializeField] private GameObject buildingPrefab;
 
     private void Update() {
         GetInput();
@@ -17,20 +16,11 @@ public class InputManager : MonoBehaviour {
             RaycastHit hit;
             if (Physics.Raycast(ray.origin, ray.direction, out hit, Mathf.Infinity, groundLayerMask)) {
                 Vector3 position = hit.point - transform.position;
-                CreateBuilding(CalculateGridPosition(position));
-                Debug.Log(position);
             }
         }
     }
 
-    private Vector3 CalculateGridPosition(Vector3 inputPosition) {
-        int x = Mathf.FloorToInt((float)inputPosition.x / _cellSize);
-        int z = Mathf.FloorToInt((float)inputPosition.z / _cellSize);
-
-        return new Vector3(x * _cellSize, 0, z * _cellSize);
-    }
-
-    private void CreateBuilding(Vector3 buildingPosition) {
-        Instantiate(buildingPrefab, buildingPosition, Quaternion.identity);
-    }
+    // private void CreateBuilding(Vector3 buildingPosition) {
+    //     Instantiate(buildingPrefab, buildingPosition, Quaternion.identity);
+    // }
 }
