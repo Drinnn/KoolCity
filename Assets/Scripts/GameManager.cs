fc6f1c1ff076e6b66c98d1a3597a4a9076dfaa9b
Drinnn <pedro_lontro@hotmail.com>
2021-04-04 13:29:04 -0300

    refactor: update game manager to use cell based grid

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -3,17 +3,23 @@ using UnityEngine;
 public class GameManager : MonoBehaviour {
     [SerializeField] public InputManager inputManager;
     [SerializeField] public PlacementManager placementManager;
+    [SerializeField] private int gridWidth;
+    [SerializeField] private int gridLength;
+
 
     private GridStructure _grid;
     private int _cellSize = 3;
 
     private void Start() {
-        _grid = new GridStructure(_cellSize);
+        _grid = new GridStructure(gridWidth, gridLength, _cellSize);
         inputManager.AddListenerOnPointerDownEvent(HandleInput);
     }
 
     private void HandleInput(Vector3 position) {
-        placementManager.PlaceBuilding(_grid.CalculateGridPosition(position));
+        Vector3 gridPosition = _grid.CalculateGridPosition(position);
+        if (!_grid.IsCellTaken(gridPosition)) {
+            placementManager.PlaceBuilding(gridPosition);
+        }
     }
 
 }
